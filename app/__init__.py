fdee767ba26ece01f7dc9abb70c9609e83e6d32d
kamauvick <kamauvick@outlook.com>
2019-09-14 15:33:42 +0300

    Add a create_app function to the init file

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+def create_app():
+    app.config.from_object(Config)
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    return app
