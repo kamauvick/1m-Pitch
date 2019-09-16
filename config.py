73647087c1ba5cc50dc5e40dbee77da99d105959
kamauvick <kamauvick@outlook.com>
2019-09-16 07:49:18 +0300

    set SQLALCHEMY_TRACK_MODIFICATIONS to True.

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 class Config:
     debug = True
+    SECRET_KEY = 'qwertyuiop'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vick:p@127.0.0.1:5432/minpitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
