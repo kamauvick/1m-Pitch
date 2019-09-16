16a0c5141ca2f094937f7eb36d20be063bf0eaf9
kamauvick <kamauvick@outlook.com>
2019-09-16 07:51:18 +0300

    Add imports for unittest and create template mothods for tests.

--- test/app_test.py
+++ test/app_test.py
@@ -1,9 +1,22 @@
 import unittest
 
+from app.models import User, Post, Comment
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class PitchTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='victor', email='victor@mail.com', password='parseword')
+        self.new_post = Post()
+        self.new_comment = Comment()
+
+    def test_user_instance(self):
+        pass
+
+    def test_post_instance(self):
+        pass
+
+    def test_comment_instance(self):
+        pass
 
 
 if __name__ == '__main__':
