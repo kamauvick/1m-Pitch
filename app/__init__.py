8e46d6c47b07fd7de64f1d5eefa0974fab18d4d1
kamauvick <kamauvick@outlook.com>
2019-09-16 07:15:43 +0300

    Update the app init file.

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from config import Config
 app = Flask(__name__)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'auth.login'
+login_manager.session_protection = 'strong'
 
 
 def create_app():
