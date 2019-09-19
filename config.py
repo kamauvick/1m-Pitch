ce91f512ea8dc010c82bda0d104893d852e9837c
kamauvick <kamauvick@outlook.com>
2019-09-19 14:15:44 +0300

    Add mail configurations to the config file.

--- config.py
+++ config.py
@@ -1,5 +1,23 @@
+import os
+
+from dotenv import load_dotenv as ld
+
+ld()
+
+
 class Config:
     debug = True
     SECRET_KEY = 'qwertyuiop'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vick:p@127.0.0.1:5432/minpitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
